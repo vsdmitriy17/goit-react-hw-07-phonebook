92ce2347dae389a4f2485433ff70b8bddaf93cfb
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2022-07-01 21:11:41 +0300

    phonebook/architecture-fix

--- src/redux/actions.js
+++
@@ -1,7 +0,0 @@
-import { createAction } from "@reduxjs/toolkit";
-
-const addContact = createAction('contacts/add');
-const deleteContact = createAction('contacts/delete');
-const filterContact = createAction('contacts/filter');
-
-export { addContact, deleteContact, filterContact };
