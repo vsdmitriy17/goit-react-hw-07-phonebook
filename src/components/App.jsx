2fb16b813a601c1dc7dd6a9ff1be7d361c29a090
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2022-07-01 20:28:50 +0300

    Revert "Revert "feature/phonebook_mockapi""
    This reverts commit 9f1222a4c6fba045eb4a265033bacf028141e4b6.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,31 +2,50 @@ import styles from './App.module.css';
 import ContactList from './contactList/ContactList.jsx';
 import ContactForm from './contactForm/ContactForm.jsx';
 import Filter from './filter/Filter.jsx';
-import { addContact, deleteContact, filterContact } from "../redux/actions.js";
-import { useSelector, useDispatch } from "react-redux";
+// import { addContact, deleteContact, filterContact } from "../redux/actions.js";
+// import { useSelector, useDispatch } from "react-redux";
+import { useState, useMemo } from 'react';
+import { useAddContactMutation } from '../redux/contactsReducer.js';
+import { useGetContactsQuery } from '../redux/contactsReducer.js';
 
 export default function App() {
-    const contacts = useSelector(state => state.contactsReducer.items.filter(contact => contact.name.toLowerCase().includes(state.contactsReducer.filter)));
-    // console.log(contacts);
-    const filter = useSelector(state => state.contactsReducer.filter);
-    const dispatch = useDispatch();
-    const formSubmit = data => {
-        dispatch(addContact(data));
-    };
-    const changeFilter = data => {
-        dispatch(filterContact(data));
-    }
-    const deleteContacts = data => {
-        dispatch(deleteContact(data));
-    }
+    // const contacts = useSelector(state => state.contactsReducer.items.filter(contact => contact.name.toLowerCase().includes(state.contactsReducer.filter)));
+    // // console.log(contacts);
+    // const filter = useSelector(state => state.contactsReducer.filter);
+    // const dispatch = useDispatch();
+    // const formSubmit = data => {
+    //     dispatch(addContact(data));
+    // };
+    // const changeFilter = data => {
+    //     dispatch(filterContact(data));
+    // }
+    // const deleteContacts = data => {
+    //     dispatch(deleteContact(data));
+    // }
+    const [addContact] = useAddContactMutation();
+  const [filter, setFilter] = useState('');
+  const { data: contacts } = useGetContactsQuery();
+
+  const createContact = async user => {
+    await addContact(user);
+  };
+
+  const filterContacts = useMemo(() => {
+    return (
+      contacts?.filter(item =>
+        item.name.toLowerCase().includes(filter.toLowerCase())
+      ) ?? []
+    );
+  }, [filter, contacts]);
+    
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Phonebook</h1>
-            <ContactForm onSubmit={formSubmit}/>
+            <ContactForm onSubmit={createContact}/>
 
             <h2 className={styles.title}>Contacts</h2>
-            <Filter filter={filter} onChangeFilter={changeFilter}/>
-            <ContactList contacts={contacts} onDeleteContact={deleteContacts}/>
+            <Filter filter={filter} onChange={setFilter}/>
+            <ContactList item={filterContacts}/>
         </div>
     );
 }
